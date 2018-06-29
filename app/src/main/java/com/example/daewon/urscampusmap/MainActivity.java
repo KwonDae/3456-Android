ce2218f7c6f299d5a08eff1eef726b1e9fc53edc
KwonDae <goodday94@cnu.ac.kr>
2018-06-30 04:34:10 +0900

    마커 여러개 띄우기

--- app/src/main/java/com/example/daewon/urscampusmap/MainActivity.java
+++ app/src/main/java/com/example/daewon/urscampusmap/MainActivity.java
@@ -25,6 +25,7 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -32,29 +33,21 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
 
 {
-
-
-
-    private GoogleMap mGoogleMap = null;
-    private Marker currentMarker = null;
 
     private static final String TAG = "googlemap_example";
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2002;
     private static final int UPDATE_INTERVAL_MS = 1000;  // 1초
     private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
-
+    String result;
     private AppCompatActivity mActivity;
-    boolean askPermissionOnceAgain = false;
-    boolean mRequestingLocationUpdates = false;
-    Location mCurrentLocatiion;
-    boolean mMoveMapByUser = true;
-    boolean mMoveMapByAPI = true;
-    LatLng currentPosition;
-
     LocationRequest locationRequest = new LocationRequest()
             .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
             .setInterval(UPDATE_INTERVAL_MS)
@@ -65,6 +58,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
+    Button restaurant, cafe, drink, place;
 
     // 위치 정보 얻는 객체
     private FusedLocationProviderClient mFusedLocationClient;
@@ -77,19 +71,79 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_main);
 
 
-        Log.d(TAG, "onCreate");
+        drink = findViewById(R.id.drink);
+        place = findViewById(R.id.place);
 
+        findViewById(R.id.restaurant).setOnClickListener(
+                new Button.OnClickListener() {
 
-
-        findViewById(R.id.Testbutton).setOnClickListener(
-                        new Button.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                NetworkTask networkTask = new NetworkTask(api_url+"spots/", null);
-                                networkTask.execute();
+                    @Override
+                    public void onClick(View view) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(result);
+                            JSONArray data = jsonObject.getJSONArray("results");
+                            int list_cnt = jsonObject.getInt("count");
+                            Double[] latitude = new Double[data.length()];
+                            Double[] logitude = new Double[data.length()];
+                            int[] id = new int[data.length()];
+                            String[] name = new String[data.length()];
+                            System.out.println(data.length());
+                            for(int i=0; i < data.length(); i++) {
+                                JSONObject object = data.getJSONObject(i);
+                                latitude[i] = object.getDouble("addr_y");
+                                logitude[i] = object.getDouble("addr_x");
+                                id[i] = object.getInt("id");
+                                name[i] = object.getString("title");
                             }
+                            for(int i =0; i< data.length(); i++) {
+                                MarkerOptions markerOptions = new MarkerOptions();
+                                markerOptions
+                                        .position(new LatLng(latitude[i],logitude[i]))
+                                        .title(name[i]);
+                                mMap.addMarker(markerOptions);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
+                    }
+                }
         );
+
+        findViewById(R.id.cafe).setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(result);
+                            JSONArray data = jsonObject.getJSONArray("results");
+                            int list_cnt = jsonObject.getInt("count");
+                            Double[] latitude = new Double[data.length()];
+                            Double[] logitude = new Double[data.length()];
+                            int[] id = new int[data.length()];
+                            String[] name = new String[data.length()];
+                            System.out.println(data.length());
+                            for(int i=0; i < data.length(); i++) {
+                                JSONObject object = data.getJSONObject(i);
+                                latitude[i] = object.getDouble("addr_y");
+                                logitude[i] = object.getDouble("addr_x");
+                                id[i] = object.getInt("id");
+                                name[i] = object.getString("title");
+                            }
+                            for(int i =0; i< data.length(); i++) {
+                                MarkerOptions markerOptions = new MarkerOptions();
+                                markerOptions
+                                        .position(new LatLng(latitude[i],logitude[i]))
+                                        .title(name[i]);
+                                mMap.addMarker(markerOptions);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+
+        Log.d(TAG, "onCreate");
         // GoogleAPIClient의 인스턴스 생성
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -100,15 +154,16 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+       SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+               .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-
-
     }
+
+
 
     @Override
     protected void onStart() {
@@ -148,12 +203,14 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             public void onSuccess(Location location) {
                 if (location != null) {
                     // 현재 위치
-                    LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    //LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    LatLng myLocation = new LatLng(36.370298, 127.345933); // 임시로 대전디폴트
                     mMap.addMarker(new MarkerOptions()
                             .position(myLocation)
                             .title("현재 위치"));
 
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+
 
                     // 카메라 줌
                     mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
@@ -178,10 +235,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-
+        NetworkTask networkTask = new NetworkTask(api_url+"spots/", null);
+        networkTask.execute();
     }
-
 
     public class NetworkTask extends AsyncTask<Void, Void, String> {
 
@@ -194,15 +250,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             this.values = values;
         }
 
-
-
         @Override
         protected String doInBackground(Void... params) {
 
-            String result; // 요청 결과를 저장할 변수.
+            // 요청 결과를 저장할 변수.
             RequestHttpConnection requestHttpURLConnection = new RequestHttpConnection();
             result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
-
             return result;
         }
 
@@ -213,7 +266,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             System.out.println("-----------------------------------");
 
             super.onPostExecute(s);
-
 
         }
     }
